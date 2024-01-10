2f7f784bf5ae29954c47afe003edc84a709899e7
xiejx <1597512995611@139.com>
2024-01-10 19:16:01 +0800

    初始化目录

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * @author xiejx
+ * @date 2024/1/10 19:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
