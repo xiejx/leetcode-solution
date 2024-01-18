c8081ee2e8364268887c0e814c46a515e077dc7d
xiejx <1597512995611@139.com>
2024-01-18 20:43:28 +0800

    添加新的解法-数位DP

--- src/main/java/com/example/problem27xx/Solution2719.java
+++ src/main/java/com/example/problem27xx/Solution2719.java
@@ -3,13 +3,16 @@ package com.example.problem27xx;
 /**
  * Hard
  * CPU: 5.81% Memory: 5.81%
+ * count0使用数位DP的方式
+ * CPU: 98.46% Memory: 55.38%
+ *
+ *
  * @author xiejx
  * @date 2024/1/16 9:37
  */
 public class Solution2719 {
     private static final int MOD = 1000000007;
     public int count(String num1, String num2, int min_sum, int max_sum) {
-        int n1 = num1.length();
         int n2 = num2.length();
         long[][][] dp = new long[max_sum + 1][n2][10];
         for (int i = 0; i <= 9 && i <= max_sum; i++) {
@@ -68,8 +71,52 @@ public class Solution2719 {
         return sum;
     }
 
+    public int count0(String num1, String num2, int min_sum, int max_sum) {
+        int n1 = num1.length();
+        int n2 = num2.length();
+        char[] numChs1 = num1.toCharArray();
+        char[] numChs2 = num2.toCharArray();
+
+        int[][] dp = new int[max_sum + 1][n2];
+        for (int i = 0; i <= max_sum; i++) {
+            for (int j = 0; j < n2; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        int b = func(0, n2, min_sum, max_sum, true, numChs2, 0, dp);
+        int a = func(0, n1, min_sum, max_sum, true, numChs1, 0, dp);
+        int temp = 0;
+        for (char ch: numChs1) {
+            temp += ch - '0';
+        }
+        temp = temp >= min_sum && temp <= max_sum ? 1 : 0;
+        return (b - a + temp + MOD) % MOD;
+    }
+
+    private int func(int i, int n, int minSum, int maxSum, boolean limit, char[] maxNum, int preSum, int[][] cache) {
+        if (i == n) {
+            return preSum >= minSum && preSum <= maxSum ? 1 : 0;
+        }
+        if (preSum > maxSum) {
+            return 0;
+        }
+        if (cache[preSum][n - i - 1] != -1 && !limit) {
+            return cache[preSum][n - i - 1];
+        }
+        long result = 0;
+        int upper = limit ? maxNum[i] - '0' : 9;
+        for (int k = 0; k <= upper; k++) {
+            result = (result + func(i + 1, n, minSum, maxSum, limit && k == upper, maxNum, preSum + k, cache)) % MOD;
+        }
+        if (!limit) {
+            cache[preSum][n - i - 1] = (int) result;
+        }
+        return (int) result;
+    }
+
     public static void main(String[] args) {
         Solution2719 test = new Solution2719();
+        long start = System.currentTimeMillis();
         // 10
         System.out.println(test.count("1", "11", 1, 8));
         // 11
@@ -82,6 +129,27 @@ public class Solution2719 {
         System.out.println(test.count("1", "10000000000000000000000", 1, 400));
         // 0
         System.out.println(test.count("8990", "9927", 1, 3));
+        // 972251498
+        System.out.println(test.count("4859473643", "30159981595", 58, 59));
+        long end = System.currentTimeMillis();
+        System.out.println("耗时： " + (end - start) + " 毫秒");
 
+        start = System.currentTimeMillis();
+        // 10
+        System.out.println(test.count0("1", "11", 1, 8));
+        // 11
+        System.out.println(test.count0("1", "12", 1, 8));
+        // 5
+        System.out.println(test.count0("1", "5", 1, 5));
+        // 99
+        System.out.println(test.count0("1", "99", 1, 18));
+        // 490000
+        System.out.println(test.count0("1", "10000000000000000000000", 1, 400));
+        // 0
+        System.out.println(test.count0("8990", "9927", 1, 3));
+        // 972251498
+        System.out.println(test.count0("4859473643", "30159981595", 58, 59));
+        end = System.currentTimeMillis();
+        System.out.println("耗时： " + (end - start) + " 毫秒");
     }
 }
